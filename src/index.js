f4f1dae6c447b18c76c90391b3ad79fcf87132e9
pharesha <gio.faresha@gmail.com>
2023-03-18 23:07:39 +0400

    commit n1

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ root.render(
   </React.StrictMode>
 );
 
+const shit = ReactDOM.createRoot(document.getElementById('shit'));
+
+shit.render(
+  <h1>sub </h1>
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
